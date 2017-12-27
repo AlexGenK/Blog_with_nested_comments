472b0ce208ac89b6befcf1ab671ec5588824b5d1
Oleksii Kozhyn <suburbakskin@gmail.com>
2017-12-27 21:08:24 +0200

    Routes adding

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :comments do
+    resources :comments
+  end
+ 
+  resources :posts do
+    resources :comments
+  end
+   
+  root to: 'posts#index'
 end
