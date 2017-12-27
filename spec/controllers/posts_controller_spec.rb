b46fdb3ef90e2946fe7ab5f5f50255b0b5a35a66
Oleksii Kozhyn <suburbakskin@gmail.com>
2017-12-27 21:39:36 +0200

    Implemented test for the posts controller

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe PostsController, type: :controller do
-
+  describe "GET index" do
+    it "assigns array of posts to @posts"
+    it "renders the index template"
+  end
+  
+  describe "GET show" do
+    it "assigns the requested post to @post"
+    it "renders the show template"
+  end
+  
+  describe "GET new" do
+    it "assigns a new post to @post"
+    it "renders the new template"
+  end
+  
+  describe "POST create" do
+    context "with valid attributes" do
+      it "saves the new post in the database"
+      it "redirects to the home page"
+    end
+    
+    context "with invalid attributes" do
+      it "does not save the new post in the database"
+      it "re-renders the new template"
+    end
+  end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Post, :type => :model do
+RSpec.describe Post, type: :model do
   it "is valid with valid attributes" do
     expect(Post.new).to be_valid
   end
