07cacc370d7190055b1d9cda922c072329de4dc6
Oleksii Kozhyn <suburbakskin@gmail.com>
2017-12-28 21:21:33 +0200

    Modify tests for the posts

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,19 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe PostsController, type: :controller do
+  
   describe "GET index" do
-    it "assigns array of posts to @posts"
-    it "renders the index template"
+    it "assigns array of posts to @posts" do
+      post_one = create(:post)
+      get :index
+      expect(assigns(:posts)).to match_array([post_one])
+    end
+
+    it "renders the index template" do
+      get :index
+      expect(response).to render_template :index
+    end
   end
   
   describe "GET show" do
     it "assigns the requested post to @post"
-    it "renders the show template"
+    it "renders the show template" do
+    end
   end
   
   describe "GET new" do
     it "assigns a new post to @post"
-    it "renders the new template"
+    it "renders the new template" do
+      get :new
+      expect(response).to render_template :new
+    end
   end
   
   describe "POST create" do
