bd8328d4cebd7ca855d85ca16a9994c4aa20f9bd
Oleksii Kozhyn <suburbakskin@gmail.com>
2017-12-28 23:34:20 +0200

    Added the ability to leave comments on posts

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,7 +6,7 @@ class CommentsController < ApplicationController
   end
  
   def create
-    @comment = @parent.comments.build(params[:comment])
+    @comment = @parent.comments.build(comment_params)
      
     if @comment.save
       redirect_to post_path(@comment.post)
@@ -22,5 +22,9 @@ class CommentsController < ApplicationController
     @parent = Comment.find_by_id(params[:comment_id]) if params[:comment_id]
      
     redirect_to root_path unless defined?(@parent)
+  end
+
+  def comment_params
+    params.require(:comment).permit(:body)
   end
 end
