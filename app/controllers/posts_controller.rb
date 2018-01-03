ff7cd23a15676bd08d1c338fc886a73caa987add
Oleksii Kozhyn <suburbakskin@gmail.com>
2018-01-03 21:24:48 +0200

    Some visual changes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
   before_action :authenticate_user!, :except => [:show, :index]
   
   def index
-    @posts=Post.all
+    @posts=Post.order(created_at: :desc)
   end
 
   def show
