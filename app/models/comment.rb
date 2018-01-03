38a2c6206235371e1097fceb21c32db1798f9d53
Oleksii Kozhyn <suburbakskin@gmail.com>
2018-01-03 22:14:36 +0200

    Deleting related entries

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,7 @@ class Comment < ApplicationRecord
   validates :body, presence: true
 
   belongs_to :commentable, polymorphic: true
-  has_many :comments, as: :commentable
+  has_many :comments, as: :commentable, dependent: :destroy
 
   def post
     return @post if defined?(@post)
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ApplicationRecord
   validates :title, :body, presence: true
   validates_date :date, on_or_after: lambda { Time.now }
   
-  has_many :comments, as: :commentable
+  has_many :comments, as: :commentable, dependent: :destroy
 end
