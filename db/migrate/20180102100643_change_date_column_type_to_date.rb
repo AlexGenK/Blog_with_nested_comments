49c30bfb31dd253a79cd6e4944daf70c3006ee99
Oleksii Kozhyn <suburbakskin@gmail.com>
2018-01-06 16:39:18 +0200

    Compacted migrations II

--- db/migrate/20180102100643_change_date_column_type_to_date.rb
+++
@@ -1,5 +0,0 @@
-class ChangeDateColumnTypeToDate < ActiveRecord::Migration[5.1]
-  def change
-     change_column :posts, :date, :date
-  end
-end
--- db/migrate/20180102140138_change_kind_column_default_to_article.rb
+++
@@ -1,5 +0,0 @@
-class ChangeKindColumnDefaultToArticle < ActiveRecord::Migration[5.1]
-  def change
-    change_column_default :posts, :kind, 'article'
-  end
-end
