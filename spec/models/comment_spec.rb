37768b766f662498ba97b0d17fa3a50dcae1fadd
Oleksii Kozhyn <suburbakskin@gmail.com>
2018-01-08 23:27:00 +0200

    Adding test for Comment#post

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -3,12 +3,18 @@ require 'rails_helper'
 RSpec.describe Comment, type: :model do
   subject { build(:comment_for_post) }
 
-  it "is valid with valid attributes" do
+  it 'is valid with valid attributes' do
     expect(subject).to be_valid
   end
 
-  it "is not valid with empty body" do
+  it 'is not valid with empty body' do
     subject.body = nil
     expect(subject).to_not be_valid
+  end
+
+  describe 'Comment#post' do
+    it 'return parental post object' do
+      expect(subject.post).to be_instance_of(Post)
+    end
   end
 end
