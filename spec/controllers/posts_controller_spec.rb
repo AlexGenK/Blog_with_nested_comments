83821d6615ea062ee71c426d3ebd1cf68a31412d
Oleksii Kozhyn <suburbakskin@gmail.com>
2018-01-14 11:24:51 +0200

    Added other tests to the Post controller

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -29,8 +29,7 @@ RSpec.describe PostsController, type: :controller do
 
   describe "GET #new" do
     before do 
-      @user = create(:user_one)
-      sign_in @user
+      sign_in create(:user_one)
     end
 
     it "assign a new instance of Post class with current date to @post" do
@@ -42,6 +41,95 @@ RSpec.describe PostsController, type: :controller do
     it "renders the new template" do
       get :new
       expect(response).to render_template("new")
+    end
+  end
+
+  describe "POST #create" do
+    before do 
+      @user = create(:user_one)
+      sign_in @user
+    end
+
+    context "with valid attributes" do
+      subject { post :create, params: { post: attributes_for(:article_from_user_one) } }
+
+      it "creates a new post" do
+        expect { subject }.to change(Post, :count).by(1)
+      end
+
+      it "redirect to the show of new post" do
+        expect(subject).to redirect_to(Post.last)
+      end
+    end
+
+    context "with invalid attributes" do
+      subject { post :create, params: { post: attributes_for(:article_from_user_one, body: nil) } }
+
+      it "does not save the new post" do
+        expect { subject }.to_not change(Post, :count)
+      end
+
+      it "re-render the new method" do
+        expect(subject).to render_template("new")
+      end
+    end
+  end
+
+  describe "POST #update" do
+    before do 
+      @user = create(:user_one)
+      sign_in @user
+      @post = create(:article_from_user_one)
+    end
+
+    context "with valid attributes" do
+      subject { put :update, params: { id: @post, post: attributes_for(:article_from_user_one, title: 'Updated', body: 'Test') } }
+
+      it "changes @post attributes" do
+        subject
+        @post.reload
+        expect(@post.title).to eq('Updated')
+        expect(@post.body).to eq('Test')
+      end
+
+      it "redirect to updated @post" do
+        expect(subject).to redirect_to(@post) 
+      end
+    end
+
+    context "with invalid attributes" do
+      subject { put :update, params: { id: @post, post: attributes_for(:article_from_user_one, title: 'Update', body: nil) } }
+
+      it "does not change @post's attributes" do
+        subject
+        @post.reload
+        expect(@post.title).to eq(attributes_for(:article_from_user_one)[:title])
+        expect(@post.body).to eq(attributes_for(:article_from_user_one)[:body])
+      end
+
+      it "re-render the edit method" do
+        subject
+        expect(subject).to render_template("edit")
+      end
+    end
+  end
+
+  describe 'DELETE #destroy' do
+    before do 
+      @user = create(:user_one)
+      sign_in @user
+      @post = create(:article_from_user_one)
+    end
+
+    subject { delete :destroy, params: { id: @post }}
+
+    it 'deletes the @post' do
+      expect { subject }.to change(Post, :count).by(-1)
+    end
+
+    it "redirects to contacts#index" do
+      subject
+      expect(subject).to redirect_to(Post)
     end
   end
 end
