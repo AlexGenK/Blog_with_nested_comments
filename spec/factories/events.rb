5ee662926ed466edea869b63c4971ef62194ae28
Oleksii Kozhyn <suburbakskin@gmail.com>
2018-02-01 23:27:59 +0200

    Modify spec for the Events model

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
-  factory :event do
-    
+  factory :event_from_user_one, class: Event do
+    title 'Event one'
+    body  'Lorem ipsum dolor sit amet.'
+    date  Time.now
+    user  'foo1@bar.com'
   end
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { build(:event_from_user_one) }
+
+  it "is valid with valid attributes" do
+    expect(subject).to be_valid
+  end
+
+  it "is not valid with empty title" do
+    subject.date = 1.day.ago
+    expect(subject).to_not be_valid
+  end
 end
